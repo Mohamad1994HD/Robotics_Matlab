7fcad600e78f54f194056aed7a3143c685caf19e
Mohamad1994HD <mohd1haddad@gmail.com>
2016-04-03 08:20:33 +0300

    Update Tcyl.m

--- Cylindrical_Transform/Tcyl.m
+++ Cylindrical_Transform/Tcyl.m
@@ -1,3 +1,6 @@
+% By Mohamad Alhadddad
+% This funciton will obtain the cylindrical transformation
+%
 function cyli = Tcyl(r, z_angle, l)
     if nargin < 3
         error('Input must be in form r (along x-axis), z_angle, l (along z-axis)');
